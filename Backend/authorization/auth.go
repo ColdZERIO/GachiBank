449ae644d69fa8a2d8b955e4ef34efb4cd6f02d8
Archil Megonovich <noob.v.katke@gmail.com>
2025-12-06 23:16:36 +0300

    New files 2

--- Backend/authorization/auth.go
+++ Backend/authorization/auth.go
@@ -1,1 +1,16 @@
 package authorization
+
+import "net/http"
+
+type UserAuth struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+func UserRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "incorrect method", http.StatusMethodNotAllowed)
+		return
+	}
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"gachibank/Backend/authorization"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
-	
+	router := chi.NewRouter()
+
+	router.Post("/auth", authorization.UserRegister)
 }
