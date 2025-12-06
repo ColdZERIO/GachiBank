22fcdf33e440ffdc1cd4c1f3f4f6547a4bcf848c
Tkachenko Dmitriy <TifaLuv@yandex.ru>
2025-12-07 01:35:09 +0300

    add main

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"context"
 	"gachibank/Backend/authorization"
+	"gachibank/database"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
+	db, err := database.DatabaseInit()
+	if err != nil {
+		log.Fatalf("Ошибка подключения к БД: %v", err)
+	}
+	defer db.Client.Disconnect(context.Background())
+
 	router := chi.NewRouter()
 
 	router.Get("/auth", authorization.GetAuth)
 	router.Post("/auth", authorization.UserRegister)
 
 	router.Handle("/*", http.FileServer(http.Dir("./")))
+
+	http.ListenAndServe(":8080", router)
 }
